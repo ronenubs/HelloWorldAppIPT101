263bec81ec4b5545452d9f8a5cb39a34d92188a5
Ron Enubs <ronenubs@gmail.com>
2020-09-15 15:05:33 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace HelloWorldApp
+{
+    partial class FrmWelcomeScreen
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNameOfUser = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnGreet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNameOfUser
+            // 
+            this.lblNameOfUser.AutoSize = true;
+            this.lblNameOfUser.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblNameOfUser.Location = new System.Drawing.Point(46, 41);
+            this.lblNameOfUser.Name = "lblNameOfUser";
+            this.lblNameOfUser.Size = new System.Drawing.Size(186, 25);
+            this.lblNameOfUser.TabIndex = 0;
+            this.lblNameOfUser.Text = "What is your name?";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtName.Location = new System.Drawing.Point(238, 38);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(397, 32);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnGreet
+            // 
+            this.btnGreet.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnGreet.Location = new System.Drawing.Point(238, 113);
+            this.btnGreet.Name = "btnGreet";
+            this.btnGreet.Size = new System.Drawing.Size(145, 37);
+            this.btnGreet.TabIndex = 2;
+            this.btnGreet.Text = "&Greet";
+            this.btnGreet.UseVisualStyleBackColor = true;
+            this.btnGreet.Click += new System.EventHandler(this.btnGreet_Click);
+            // 
+            // FrmWelcomeScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGreet);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblNameOfUser);
+            this.Name = "FrmWelcomeScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WELCOME APPLICATION";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNameOfUser;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnGreet;
+    }
+}
+
